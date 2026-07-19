668d3c31f91d522ceac87a322f81a09204f44eaa
Nitish Choudhary <nitishchoudharyc2@gmail.com>
2026-07-19 09:26:50 +0530

    chore: initialize frontend with Vite and project structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <section>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1>Hello World</h1>
+          </div>
+        </div>
+      </div>
+     </section>
+      
+    </>
+  )
+}
+
+export default App
