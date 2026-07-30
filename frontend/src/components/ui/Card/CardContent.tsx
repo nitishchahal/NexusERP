f33a27c9be0f2fe2c1ed490f81bdb1b2c1c2d81a
Nitish Choudhary <158278486+nitishchahal@users.noreply.github.com>
2026-07-30 23:26:45 +0530

    CHanges in CardContent

--- frontend/src/components/ui/Card/CardContent.tsx
+++ frontend/src/components/ui/Card/CardContent.tsx
@@ -4,17 +4,10 @@ import { cardContentStyles } from "./card.styles";
 const CardContent = ({
   children,
   className = "",
-}: CardSectionProps) => {
-  return (
-    <div
-      className={`
-        ${cardContentStyles}
-        ${className}
-      `}
-    >
-      {children}
-    </div>
-  );
-};
+}: CardSectionProps) => (
+  <div className={`${cardContentStyles} ${className}`}>
+    {children}
+  </div>
+);
 
 export default CardContent;
